6ecb76dc79b74915abb5485650fc4e530b1401f3
Ilya <efimov4242@mail.ru>
2022-11-30 22:45:25 +0400

    добавил метод создания массива строк

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,27 @@
 ﻿/*
 Задача. Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. Первоеачальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 */
+
+string[] GetCreateArray()
+{
+	string[] array = new string[5];
+	int number = 1;
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.WriteLine("Введите строку: " + number);
+		array[i] = Console.ReadLine();
+		number++;
+	}
+	return array;
+}
+
+void PrintChar(string[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		if((array[i].Length) <= 3)
+		Console.Write(array[i] + " ");
+	}
+}
+
+PrintChar(GetCreateArray());
